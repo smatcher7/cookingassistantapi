c52140e10f70dbd6e929a3fa2286a8f0bb0aac89
Rui Ferreira <ruiferreira91@gmail.com>
2024-11-24 17:11:12 +0000

    changes

--- CookingAssistant/Program.cs
+++ CookingAssistant/Program.cs
@@ -7,14 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<ICookerHandler<Recipe, Ingredient>, CookerHandler>();
 // Add services to the container.
-builder.Services.AddGrpc().AddJsonTranscoding();;
+builder.Services.AddGrpc().AddJsonTranscoding();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] { "en-US", "pt-PT" };
+    options.SetDefaultCulture(supportedCultures[0]);
+    options.AddSupportedCultures(supportedCultures);
+    options.AddSupportedUICultures(supportedCultures);
+});
 
 var app = builder.Build();
 
+app.UseGrpcWeb();
 app.UseRequestLocalization();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<CookerService>();
+app.MapGrpcService<CookerService>().EnableGrpcWeb();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
--- CookingAssistant/obj/Debug/net9.0/CookingAssistant.AssemblyInfo.cs
+++ CookingAssistant/obj/Debug/net9.0/CookingAssistant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookingAssistant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83e7b6a85b3b889712df2a16f62e1c8fd3513a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08fce38872ea2bb6917921e117c2a307858ba531")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookingAssistant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookingAssistant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
